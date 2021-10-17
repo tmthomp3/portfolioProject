5d76965b325e157a5901322f4c6b9df82061bdc9
Telissa Thompson <tmthomp3@gmail.com>
2021-10-17 19:44:23 -0400

    added login Modal

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+/* login modal*/ 
+$(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+});
